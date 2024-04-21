fe1283be81094776a9ef012849c6578a5ec57399
kananbayramli <bayramli-kenan@mail.ru>
2024-04-21 19:33:47 +0400

    Business project

---
+++ Concrete/ProductManager.cs
@@ -0,0 +1,49 @@
+﻿using shopapp.business.Abstract;
+using shopapp.data.Abstract;
+using shopapp.data.Concrete.EfCore;
+using shopapp.entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shopapp.business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        private IProductRepository _productRepository;
+
+        public ProductManager(IProductRepository productRepository)
+        {
+            this._productRepository = productRepository;
+        }
+        public void Create(Product entity)
+        {
+            //yoxlamalar
+            _productRepository.Create(entity);
+        }
+
+        public void Delete(Product entity)
+        {
+            //yoxlamalar
+            _productRepository.Delete(entity);
+        }
+
+        public List<Product> GetAll()
+        {
+            //yoxlamalar
+            return _productRepository.GetAll();
+        }
+
+        public Product GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
