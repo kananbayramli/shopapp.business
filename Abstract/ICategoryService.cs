1d46f6051cc9ce1a07133e0d38a3f140550d61bd
kananbayramli <bayramli-kenan@mail.ru>
2024-04-22 17:59:18 +0400

    Dynamic category menu

---
+++ Abstract/ICategoryService.cs
@@ -0,0 +1,19 @@
+using shopapp.entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shopapp.business.Abstract
+{
+    public interface ICategoryService
+    {
+        Category GetById(int id);
+        List<Category> GetAll();
+        void Create(Category entity);
+        void Update(Category entity);
+        void Delete(Category entity);
+        
+    }
+}
