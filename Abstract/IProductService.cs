351f2045138450cee1f009fb5ee69bfde6c638cc
kananbayramli <bayramli-kenan@mail.ru>
2024-04-25 15:53:34 +0400

    Products link

--- Abstract/IProductService.cs
+++ Abstract/IProductService.cs
@@ -10,7 +10,7 @@ namespace shopapp.business.Abstract
     public interface IProductService
     {
         Product GetById(int id);
-        Product GetProductDetails(int id);
+        Product GetProductDetails(string url);
         List<Product> GetProductsByCategory(string name);
         List<Product> GetAll();
         void Create(Product entity);
