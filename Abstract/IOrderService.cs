c27868333974bf1a8eeef158fa0aa3adb7656af3
kananbayramli <bayramli-kenan@mail.ru>
2024-05-14 14:48:36 +0400

    Registering an Order / With Iyzipay

---
+++ Abstract/IOrderService.cs
@@ -0,0 +1,14 @@
+﻿using shopapp.entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shopapp.business.Abstract
+{
+    public interface IOrderService
+    {
+        void Create(Order entity);
+    }
+}
---
+++ Concrete/OrderManager.cs
@@ -0,0 +1,25 @@
+﻿using shopapp.business.Abstract;
+using shopapp.data.Abstract;
+using shopapp.entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shopapp.business.Concrete
+{
+    public class OrderManager : IOrderService
+    {
+        private readonly IOrderRepository _orderRepository;
+        public OrderManager(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        public void Create(Order entity)
+        {
+            _orderRepository.Create(entity);
+        }
+    }
+}
